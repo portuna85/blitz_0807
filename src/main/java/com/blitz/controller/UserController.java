6e68b6e58f00495d82053e2dce8a2466fdedd1d5
portuna85 <portuna85@gmail.com>
2022-08-01 23:31:45 +0900

    Main Class Finder

--- src/main/java/com/blitz/controller/UserController.java
+++ src/main/java/com/blitz/controller/UserController.java
@@ -64,7 +64,7 @@ public class UserController {
             return "user/user-join";
         }
         userService.userJoin(dto);
-        return "redirect:auth/login";
+        return "redirect:/login";
     }
 
     /** 일반 로그인 */

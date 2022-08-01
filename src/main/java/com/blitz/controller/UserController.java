4dd075e5f1588b96d571f9f37cd934cea5d09a32
portuna85 <portuna85@gmail.com>
2022-08-01 23:16:03 +0900

    Main Class Finder

--- src/main/java/com/blitz/controller/UserController.java
+++ src/main/java/com/blitz/controller/UserController.java
@@ -61,12 +61,10 @@ public class UserController {
                 model.addAttribute(key, validatorResult.get(key));
             }
             /* 회원가입 페이지로 다시 리턴 */
-            return "user/user-join";
-            //return "/user/user-join";
+            return "/user/user-join";
         }
         userService.userJoin(dto);
-        return "redirect:auth/login";
-        //return "redirect:/auth/login";
+        return "redirect:/auth/login";
     }
 
     @GetMapping("/auth/login")
@@ -75,7 +73,7 @@ public class UserController {
                         Model model) {
         model.addAttribute("error", error);
         model.addAttribute("exception", exception);
-        return "user/user-login";
+        return "/user/user-login";
         //return "/user/user-login";
     }
 

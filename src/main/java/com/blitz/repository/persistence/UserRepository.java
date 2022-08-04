4ea242ca139be2fb2a4f392fa812bf2296b76338
portuna85 <portuna85@gmail.com>
2022-08-04 18:07:34 +0900

    Update

--- src/main/java/com/blitz/repository/persistence/UserRepository.java
+++ src/main/java/com/blitz/repository/persistence/UserRepository.java
@@ -2,6 +2,8 @@ package com.blitz.repository.persistence;
 
 import com.blitz.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
